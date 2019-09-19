aa273c4814e11e4258b3269f142635e55ca4bd10
Henrique Mariz <ham2@cin.ufpe.br>
2019-09-19 20:45:00 -0300

    Step 1 and Step2

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -2,11 +2,27 @@ package br.ufpe.cin.android.podcast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.UiThread
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        doAsync{
+            val xmlLink= URL("https://www.genkidama.com.br/blog/category/anikencast/feed/podcast/").readText()
+            val itensFeed = Parser.parse(xmlLink)
+            uiThread{
+                toast(itensFeed.toString())
+            }
+        }
+
+
     }
 }
