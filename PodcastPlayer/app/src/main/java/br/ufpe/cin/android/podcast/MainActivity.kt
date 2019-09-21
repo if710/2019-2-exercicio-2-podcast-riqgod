f45b499d4168c3b594ec6feb613081c4d8b43ff0
Henrique Mariz <ham2@cin.ufpe.br>
2019-09-20 22:28:50 -0300

    STEP 6 not working properly yet, catch not catching

--- PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
+++ PodcastPlayer/app/src/main/java/br/ufpe/cin/android/podcast/MainActivity.kt
@@ -8,9 +8,11 @@ import androidx.annotation.UiThread
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.ctx
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
+import java.lang.Exception
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
@@ -18,23 +20,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val database = ItemFeedDB.getDatabase(this)
-        doAsync{
-            val xmlLink= URL("https://www.genkidama.com.br/blog/category/anikencast/feed/podcast/").readText()
-            val itemsFeed = Parser.parse(xmlLink)
-            uiThread{
-                recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
-                recyclerView.adapter = CustomAdapter(itemsFeed, this@MainActivity )
-                recyclerView.addItemDecoration(
-                    DividerItemDecoration(this@MainActivity,LinearLayoutManager.VERTICAL)
-                )
+        val database = ItemFeedDB.getDatabase(this@MainActivity)
+        var itemsFeed = listOf<ItemFeed>()
+        doAsync {
+            try{
+                val xmlLink = URL("https://www.genkidama.com.br/blog/category/anikencast/feed/podcast/").readText()
+                itemsFeed = Parser.parse(xmlLink)
 
+                for (item in itemsFeed) {
+                    database.itemFeedDAO().addItem(item)
+                    println(item)
+                }
+
+            }catch (e: Exception){
+                Toast.makeText(ctx,"Internet error! loading stored data...",Toast.LENGTH_SHORT).show()
+                println("Internet error! loading stored data..."+e.toString())
+                itemsFeed = database.itemFeedDAO().allItems()
+            } finally{
+                uiThread{
+                    recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                    recyclerView.adapter = CustomAdapter(itemsFeed, this@MainActivity )
+                    recyclerView.addItemDecoration(
+                        DividerItemDecoration(this@MainActivity,LinearLayoutManager.VERTICAL)
+                    )
+                }
             }
-            for (item in itemsFeed) {
-                database.itemFeedDAO().addItem(item)
-                println(item)
-            }
+
         }
-
     }
 }
